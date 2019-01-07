beaeac036f0f8ac29bf532effe9c7183ea137e18
Prateek Maheshwari <pmaheshwari@apache.org>
2019-01-07 15:18:53 -0800

    Clean up dependencies.

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,9 +1,5 @@
 package util;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Slf4jReporter;
-import com.codahale.metrics.jmx.JmxReporter;
-import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 
 import java.io.File;
@@ -13,25 +9,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Comparator;
-import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
 
 public class Util {
-  public static void initMetrics(MetricRegistry metrics, Logger logger) {
-    final Slf4jReporter logReporter = Slf4jReporter.forRegistry(metrics)
-        .outputTo(logger)
-        .convertRatesTo(TimeUnit.SECONDS)
-        .convertDurationsTo(TimeUnit.NANOSECONDS)
-        .build();
-    logReporter.start(5, TimeUnit.SECONDS);
-
-    final JmxReporter jmxReporter = JmxReporter.forRegistry(metrics)
-        .convertRatesTo(TimeUnit.SECONDS)
-        .convertDurationsTo(TimeUnit.NANOSECONDS)
-        .build();
-    jmxReporter.start();
-  }
-
   public static long readFile(Path filePath) {
     try {
       return Files.isReadable(filePath)
